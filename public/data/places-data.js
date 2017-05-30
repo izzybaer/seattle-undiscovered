720078a0e53fc728dd38d59c68ad5b403e66626d
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-05-30 13:40:52 -0700

    newPLaces has only 10 objects

--- public/data/places-data.js
+++ public/data/places-data.js
@@ -1,9 +1,9 @@
 var markers = [
-  [0, 'Columbian Tower', 47.604514, -122.330484, 'points-of-interest', 'This is the content'],
   [1, 'Kells Irish Pub', 47.610594, -122.342332, 'restaurants', 'This is a bar'],
   [2, 'test', 47.610594, -122.342332, 'activities', 'ipsum'],
   [3, 'slkdjfsld', 47.610594, -122.342332, 'nightlife', 'sldkjfsl'],
-  [4, 'bob', 47.610594, -122.342332, 'points-of-interest', 'bob']
+  [4, 'bob', 47.610594, -122.342332, 'points-of-interest', 'bob'],
+  [5, 'ChIJi-OPx7BqkFQRLDWjAaSJ6ss']
 
 
 
